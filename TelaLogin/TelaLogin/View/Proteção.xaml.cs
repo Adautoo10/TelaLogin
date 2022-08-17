5742a5fbb7446ea3d5572ebe85c2669ea26fd748
Adautoo10 <adauto.facin05@gmail.com>
2022-08-17 15:10:00 -0300

    Adicionar arquivos de projeto.

---
+++ TelaLogin/TelaLogin/View/Proteção.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TelaLogin.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Proteção : ContentPage
+    {
+        public Proteção()
+        {
+            InitializeComponent();
+        }
+    }
+}
